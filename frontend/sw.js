7218f7a8ebcb38983be23d2ea5ed56e4da09a95c
selvaganesh19 <selvaganesh.al22@bitsathy.ac.in>
2026-03-05 13:39:16 +0530

    added service worker file

---
+++ frontend/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "electricity-bill-pwa-v2";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon/favicon-16x16.png",
+  "./icon/favicon-32x32.png",
+  "./icon/favicon-96x96.png",
+  "./icon/android-icon-144x144.png",
+  "./icon/android-icon-192x192.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  if (req.mode === "navigate") {
+    event.respondWith(fetch(req).catch(() => caches.match("./index.html")));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        if (res && res.ok && new URL(req.url).origin === self.location.origin) {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+        }
+        return res;
+      });
+    })
+  );
+});
